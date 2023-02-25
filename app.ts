f5c474fdaa3d04c7723784d7243dc6783bc8f2f2
ArtemKorotkov321 <124918352+ArtemKorotkov321@users.noreply.github.com>
2023-02-25 12:29:47 +0300

    Лаб.1 задание 1

--- app.ts
+++ app.ts
@@ -1,38 +1,54 @@
 ﻿const fs = require('fs');            
-const Path = './text_file.txt';
-const textToSearch = "korotkov";
+const file = process.argv[2];
+const textToSearch = process.argv[3];
 
 
-function fileExist(): Boolean {
-    if (fs.existsSync(Path)) {
-        return true;
-    } else {
-        console.log("File not found");
-        return false;
+function checkArguments() {
+    if (process.argv.length != 4) {
+        console.log("Invalid number of arguments\nUsage <program> <file name> <text to search>");
+        process.exit(1);
     }
 }
 
+
+function fileExist() {
+    if (!fs.existsSync(file)) {
+        console.log("File not found");
+        process.exit(1);
+    }
+}
+
+
+function getTextFile() {
+    let textFile: String = fs.readFileSync(file, "utf8");
+    if (textFile.length == 0) {
+        console.log("Empty file");
+        process.exit(1);
+    }
+    let textFileLines = textFile.split(/\r?\n/);
+    return textFileLines;
+}
+
+
 function searchText(textToSearch: string) {
     let countLines = 0;
-    textFileLines.forEach(function (line, index) {
-        console.log("Строка №", index + 1, line);
-        if (line.search(textToSearch) > 0) {
+    getTextFile().forEach(function (line, index) {
+        if (line.search(textToSearch) != -1) {
             countLines += 1;
             console.log(index + 1);
         }
     });
     if (countLines == 0) {
         console.log("Text not found");
+        process.exit(1);
     }
 }
 
 
+function main() {
+    checkArguments();
+    fileExist();
+    searchText(textToSearch);
+}
 
-
-var textFile: String = fs.readFileSync("text_file.txt", "utf8");
-var textFileLines = textFile.split(/\r?\n/);
-
-
-fileExist();
-searchText("string");
-
+main();
