f50720757905ec1a7f0944886fe3ed3067b45ff2
ArtemKorotkov321 <124918352+ArtemKorotkov321@users.noreply.github.com>
2023-02-26 21:28:40 +0300

    Лаб.1 задание 2 + батник

---
+++ app.ts
@@ -0,0 +1,49 @@
+﻿const binNum = process.argv[2];
+
+
+function checkArgumentIsNumber() {
+    if (process.argv.length < 3 || process.argv.length > 3) {
+        console.log("Error: enter one binary argument");
+        process.exit(1);
+    }
+    else if (isNaN(binNum as any)) {
+        console.log("Error: argument is not a number\nUse an integer");
+        process.exit(1);
+    }
+    else if (Number(binNum) < 0 || Number(binNum) > Number(Math.pow(2, 31).toString(2))) {
+        console.log("Error: enter a binary number from 0 to 2^32-1");
+        process.exit(1);
+    }
+}
+
+
+function checkNumberIsBin() {
+    for (let ch = 0; ch < binNum.length; ch++) {
+        if (!(Number(binNum[ch]) == 0 || Number(binNum[ch]) == 1)) {
+            console.log("Error: enter a binary number");
+            process.exit(1);
+        }
+    }
+}
+
+
+function convertingNumber() {
+    let decimalNumber = 0;
+    let degree = 0;
+    for (let index = binNum.length; index > 0; index--) {
+        let lastChar = Number(binNum[index - 1]);
+        decimalNumber += lastChar * Math.pow(2, degree);
+        degree++;
+    }
+    console.log("Результат: ", decimalNumber);
+}
+
+
+function main() {
+    checkArgumentIsNumber();
+    checkNumberIsBin();
+    convertingNumber();  
+}
+
+
+main();
--- test.bat
+++ test.bat
@@ -1,22 +1,33 @@
 @echo off
 set program=node app.js
 
-rem Запуск без аргументов
-%program% 
-if ERRORLEVEL 1 (echo Test 1 passed. Incorrect number of arguments) else (echo Arguments test fail exit 1) 
+rem Запуск программы без аргумента
+%program%
+if ERRORLEVEL 1 (echo Test 1 passed) else (echo Test 1 failed exit 1)
 
-rem Текстовый файл не существует
-%program% tests/432.txt pass
-if ERRORLEVEL 1 (echo Test 2 passed. File not found) else (echo FileExist test fail exit 1)
+rem Запуск программы с несколькими аргументами
+%program% 1 3 5
+if ERRORLEVEL 1 (echo Test 2 passed) else (echo Test 2 failed exit 1)
 
-rem Текстовый файл пустой
-%program% tests/empty.txt 1
-if ERRORLEVEL 1 (echo Test 3 passed. File is empty) else (echo EmptyFile test fail exit 1)
+rem Запуск программы со строкой-аргументом
+%program% string
+if ERRORLEVEL 1 (echo Test 3 passed) else (echo Test 3 failed exit 1)
 
-rem Запуск с правильными параметрами
-%program% tests/test.txt dog
-if NOT ERRORLEVEL 0 (echo Test 4 fail. Text not found exit 1) else (echo Test 4 passed. Text found)
+rem Запуск программы с отрицательным числом
+%program% -1
+if ERRORLEVEL 1 (echo Test 4 passed) else (echo Test 4 failed exit 1)
 
+rem Запуск программы с числом больше допустимого
+%program% 1000000000000000000000000000000000000000000000000000000
+if ERRORLEVEL 1 (echo Test 5 passed) else (echo Test 5 failed exit 1)
+
+rem Запуск программы с небинарным числом
+%program% 23
+if ERRORLEVEL 1 (echo Test 6 passed) else (echo Test 6 failed exit 1)
+
+rem Запуск программы с корректным аргументом
+%program% 1001
+if NOT ERRORLEVEL 0 (echo Test 7 failed) else (echo Test 7 passed)
+
+echo Tests OK
 exit 0
-
-
